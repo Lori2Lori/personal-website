70e09f545a873d7a29a5221faaefae4b427a62aa
lori2lori <cieslinska.dorota@gmail.com>
2015-03-28 12:44:22 +0100

    Shortened version of jquery-events script

--- assets/scripts/jquery-events.js
+++ assets/scripts/jquery-events.js
@@ -1,10 +1,8 @@
-add_well = function() {
-  paragraphs = $('p')
-  paragraphs.addClass("well")
-}
-
-$("button").on("click", add_well)
+$("button").on("click", function() {
+  $('p').addClass("well")
+})
 
 // on() is a function that takes 2 parameters:
 // 1. first is an event name
 // 2. second is a function
+// 'p' means paragraphs in html
